b5c1d7d1e9a8de2cf63567919983d369d25cc55b
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-07-06 03:26:50 +0200

    refactor

--- shop.rb
+++ shop.rb
@@ -2,7 +2,7 @@ class Product
 
 attr_accessor :id, :name, :price
 	
-	@@id_max = 0
+	@@id_max = 0 # automatically generate unique id
 	def initialize name, price
 		@id = @@id_max += 1
 		@name = name
@@ -14,21 +14,19 @@ class WareHouse
 
 attr_accessor :products_count
 
-	def initialize products_hash
+	def initialize products_hash	# container for Product => Quantity pairs
 	 @products_count = products_hash
 	end
 
-	def fetch name
+	def fetch name 
 		product =  @products_count.keys.find{ |a| a.name == name}
 		if available? product
 			@products_count[product] -= 1 
 			product 
-		else
-			false
 		end
 	end
 
-	def back name
+	def back name # put product back to the warehouse
 		product =  @products_count.keys.find{ |a| a.name == name}
 		@products_count[product] += 1
 	end
@@ -37,8 +35,6 @@ private
 	def available? product
 		unless product.nil?
 			products_count[product] > 0 				
-		else
-			false
 		end
 	end
 end
@@ -54,7 +50,7 @@ attr_accessor :basket, :warehouse
 
 	def add name
 		product = warehouse.fetch name
-		(product)? @basket <<(product) : product   
+		(product)? @basket <<(product) : product   # if product exist add it to the basket
 	end
 
 	def remove name
@@ -71,10 +67,6 @@ attr_accessor :basket, :warehouse
 
 	def price_netto
 		basket.inject(0){ |sum, n| sum + n.price }
-	end
-
-	def find_product name
-		basket.find{ |a| a.name == name}
 	end
 
 	def receit
@@ -99,4 +91,4 @@ basket = ShopBasket.new(  WareHouse.new( { Product.new("milk", 5) => 9,  Product
 30.times { basket.add "milk"; basket.add "bread" }
 30.times{ basket.remove("milk") }
 basket.receit
-puts basket.warehouse.inspect
+
